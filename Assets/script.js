c45e4b4f07d0a47a0ea191b230d6db84740b7f01
Joseph Soria <josephsoria6@gmail.com>
2020-12-16 23:37:49 -0600

    Added event listeners to buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,11 @@
 var startBtn = document.getElementById(`start`);
 var highscoreBtn = document.getElementById(`highscore`);
+
+highscoreBtn.addEventListener('click', function() {
+    console.log("ok");
+})
+
+startBtn.addEventListener('click', function() {
+    console.log("ok");
+})
+
