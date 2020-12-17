0729c8a2950622d2937a9553e65397e6b9a8eeef
Joseph Soria <josephsoria6@gmail.com>
2020-12-17 00:19:28 -0600

    Added function newQuestion()

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,41 @@
 var startBtn = document.getElementById(`start`);
 var highscoreBtn = document.getElementById(`highscore`);
 var timeEl = document.getElementById(`timer`)
+var questionBox = document.getElementById(`questions`)
+
 
 highscoreBtn.addEventListener('click', function() {
     console.log('ok');
 })
 
+function newQuestion() {
+    var newList = document.createElement(`p`);
+    newList.textContent = `Hello World`;
+    questionBox.appendChild(newList);
+}
+
 startBtn.addEventListener('click', function() {
     let secondsLeft = 30;
     timeEl.textContent = `Time: ${secondsLeft}`;
+    newQuestion();
+    
     var timerInterval = setInterval(function() {
         timeEl.textContent = `Time: ${secondsLeft}`;
         if(secondsLeft === 0) {
+            alert(`Time's up!`);
             clearInterval(timerInterval);
         }
         secondsLeft--
-
-
     }, 1000)
+
+
+
 })
+
+
+
+
+
+
+
 
