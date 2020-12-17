7a51deaa6db75021822fba9f4047655de1f8dfd4
Joseph Soria <josephsoria6@gmail.com>
2020-12-16 23:50:36 -0600

    Changed text content of timer id when start btn is clicked

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,19 @@
 var startBtn = document.getElementById(`start`);
 var highscoreBtn = document.getElementById(`highscore`);
+var timeEl = document.getElementById(`timer`)
 
 highscoreBtn.addEventListener('click', function() {
-    console.log("ok");
+    console.log('ok');
 })
 
 startBtn.addEventListener('click', function() {
-    console.log("ok");
+    let secondsLeft = 30;
+    timeEl.textContent = `Time: ${secondsLeft}`;
+    var timerInterval = setInterval(function() {
+        // secondsLeft--
+        
+
+    })
+    console.log('ok');
 })
 
