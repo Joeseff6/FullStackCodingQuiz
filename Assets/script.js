65465f477d5dfa20082cc7f66ad634749b5d0280
Joseph Soria <josephsoria6@gmail.com>
2020-12-18 23:33:31 -0600

    Erased selectAnswer callback function

--- Assets/script.js
+++ Assets/script.js
@@ -3,12 +3,7 @@ var highscoreBtn = document.getElementById(`highscore`);
 var timeEl = document.getElementById(`timer`)
 var questionBox = document.getElementById(`questions`)
 var question1 = { question: `What does HTML stand for?`, answer1: `Hypertext Mocking Language`, answer2: `Hypertext Markup Language`, answer3: `Hypertalking Mic Language`, answer4: `Hypertext Memory Log`};
-var correctAnswers = [`HyperText Markup Language`, `To render a document so it can be displayed in a web browser.`, `Javascript`, `A web application is a webpage that is interactive through Javascript.`, 
-`Cascading Style Sheets`, `To style HTML files.`, `HTML, CSS, Javascript`, `Internal`, `Inline`, `Selectors`, `Attributes`, `Classes can be used on multiple elements to style them at one time, while id's must be unique to the element.`, 
-`.`, `#`, `To add functionality to a webpage.`, `<script>`, `for loops take an initial expression and runs through iterations until a conditional expression evaluates false.`, 
-`{}`, `The element doesn't need to be wrapped in &lt>.`, `Functions can be called and passed once a function is declared.`, `Anonymous`, 
-`Document Object Model`, `DOM can be used to select elements in Javascript for assignment.`, `Server-side Javascript allows back-end features, while client-side Javascript only interacts with the user's webpage on their local machine.`, 
-`Inspector`];
+
 
 highscoreBtn.addEventListener('click', function() {
     console.log('ok');
@@ -17,17 +12,7 @@ highscoreBtn.addEventListener('click', function() {
 
 function selectAnswer() {
     questionBox.addEventListener(`click`, function(event) {
-        var elementText = event.target.innerText;
-        elementText = new String(elementText);
-        console.log(elementText)
-        if (correctAnswers.includes(elementText)) {
-        // if (correctAnswers.includes("`" + elementText + "`") === true) {
-            console.log(`pass`);
-        } else {
-            console.log(`fail`);
-        }
-        return elementText;
-    })
+    });
 }
 
 function startQuestion() {
