423ccef9648bb3da82ff208aa2445932c4f02055
Joseph Soria <josephsoria6@gmail.com>
2020-12-21 21:12:46 -0600

    Adjusted code to present questions in order instead of random

--- Assets/script.js
+++ Assets/script.js
@@ -11,8 +11,16 @@ highscoreBtn.addEventListener(`click`, function() {
 })
 
 startBtn.addEventListener(`click`, function() {
+    // Collect the score array in local storage when start button is clicked
     getScore()
 
+    function getScore() {
+        var storedScore = JSON.parse(localStorage.getItem(`Quiz scores`));
+    
+        if (storedScore !== null) {
+            scores = storedScore;
+        }
+    }
 
     // Remove start button to replace with Player score.
     startContainer.removeChild(startBtn);
@@ -20,39 +28,18 @@ startBtn.addEventListener(`click`, function() {
     scoreText.textContent = `Score: 0`;
     startContainer.appendChild(scoreText);
 
-    // Set up questions and answers when start button is clicked
-    var question = document.getElementById(Math.floor(Math.random()*Math.floor(25)));
-    question.removeAttribute(`class`, `d-none`);
-    var answerchoices = question.querySelectorAll('p');
-
     // Set player score to zero
     var score = 0;
 
-    // Change question after an answer choice is selected
-    function newQuestion() {
-        scoreText.textContent = `Score: ${score}`
-        question.setAttribute(`class`, `d-none`)
-        question = document.getElementById(Math.floor(Math.random()*Math.floor(25)+1));
-        question.removeAttribute(`class`, `d-none`);
-        answerchoices = question.querySelectorAll('p');
-    }
-
-    // Add functionality for when an answer choice is clicked on
-    function clickAnswer(event) {
-        if (event.target.dataset.answer === `correct`) {
-            score++
-            alert(`Correct!`);
-        } else {
-            secondsLeft--
-            alert(`Incorrect...`)
-        }
-        newQuestion()
-    }
+    // Set up questions and answers when start button is clicked
+    var questionNumber = 1;
+    var question = document.getElementById(questionNumber);
+    question.removeAttribute(`class`, `d-none`);
+    var answerchoices = question.querySelectorAll('p');
 
     // Store time information for an interval function
     let secondsLeft = 60;
     timeEl.textContent = `Time: ${secondsLeft}`;
-
 
     // Set a time interval to allow answers to be selected once the start button is clicked
     var timerInterval = setInterval(function() {
@@ -70,21 +57,32 @@ startBtn.addEventListener(`click`, function() {
         question.addEventListener('click', clickAnswer)
     }, 1000)
 
+    // Add functionality for when an answer choice is clicked on
+    function clickAnswer(event) {
+        if (event.target.dataset.answer === `correct`) {
+            score++
+            alert(`Correct!`);
+        } else {
+            secondsLeft--
+            alert(`Incorrect...`)
+        }
+        newQuestion()
+    }
 
-    
-    
+    // Change question after an answer choice is selected
+    function newQuestion() {
+        scoreText.textContent = `Score: ${score}`
+        questionNumber += 1;
+        question.setAttribute(`class`, `d-none`)
+        question = document.getElementById(questionNumber);
+        question.removeAttribute(`class`, `d-none`);
+        answerchoices = question.querySelectorAll('p');
+    }
+
+    // Store score function for when the timer runs out
     function storeScore () {
         scores.push(score);
         localStorage.setItem("Quiz scores", JSON.stringify(scores));
     }
 
-
-
-    function getScore() {
-        var storedScore = JSON.parse(localStorage.getItem(`Quiz scores`));
-    
-        if (storedScore !== null) {
-            scores = storedScore;
-        }
-    }
 })
