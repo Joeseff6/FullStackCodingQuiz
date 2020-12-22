b4656022ebfa64d951328dacd8e9907b12b56a25
Joseph Soria <josephsoria6@gmail.com>
2020-12-21 18:03:22 -0600

    Edited all files to run localstorage methods properly

--- Assets/highscoreScript.js
+++ Assets/highscoreScript.js
@@ -1,17 +1,69 @@
-var scoreSubmit = document.getElementById(`submitForm`);
-var rowContainer = document.getElementById(`rowContainer`);
+const submitForm = document.getElementById(`submitForm`);
+const formValue = document.getElementById(`enterInitials`);
+const rowContainer = document.getElementById(`rowContainer`);
+var initials = [];
+var scores = [];
 
-scoreSubmit.addEventListener(`submit`, function(event) {
+getStoredInfo();
+
+function rendorStoredInfo() {
+    for (let i = 0; i < initials.length; i++) {
+        var initial = initials[i];
+        var score = scores[i];
+        var rowEl = document.createElement('tr')
+        var td1 = document.createElement(`td`);
+        var td2 = document.createElement(`td`);
+        td1.textContent = initial;
+        td2.textContent = score;
+
+        rowContainer.appendChild(rowEl);
+        rowEl.appendChild(td1);
+        rowEl.appendChild(td2);
+    }
+}
+
+function getStoredInfo() {
+    var storedInitials = JSON.parse(localStorage.getItem("initials"));
+    var storedScores = JSON.parse(localStorage.getItem("Quiz scores"));
+
+
+    if (storedInitials !== null) {
+        initials = storedInitials;
+    }
+
+    if (storedScores !== null) {
+        scores = storedScores;
+    }
+
+    rendorStoredInfo()
+
+}
+
+
+
+
+function storeInfo() {
+    localStorage.setItem(`initials`, JSON.stringify(initials));
+    localStorage.setItem(`Quiz scores`, JSON.stringify(scores));
+}
+
+submitForm.addEventListener(`submit`, function(event) {
     event.preventDefault()
-    var rowEl = document.createElement('tr')
-    rowEl.setAttribute(`id`, `newRow`)
-    rowContainer.appendChild(rowEl);
-    var newInitial = document.createElement(`td`);
-    var newScore = document.createElement(`td`);
-    newInitial.textContent = `JS`
-    newScore.textContent = `Score`
-    rowEl.appendChild(newInitial);
-    rowEl.appendChild(newScore);
 
+    var newInitial = formValue.value.trim();
 
+    if (newInitial === ``) {
+        return;
+    }
+
+    initials.push(newInitial);
+    formValue.value = `Go back to play again`;
+
+    storeInfo();
+
+    while (rowContainer.firstChild) {
+        rowContainer.removeChild(rowContainer.firstChild)
+    }
+
+    rendorStoredInfo();
 })
--- Assets/script.js
+++ Assets/script.js
@@ -3,13 +3,24 @@ var highscoreBtn = document.getElementById(`highscore`);
 var timeEl = document.getElementById(`timer`)
 var questionBox = document.getElementById(`questions`)
 var startContainer = document.getElementById(`startContainer`);
+var scores = [];
+
+
 
 startBtn.addEventListener(`click`, function() {
+    getScore()
+
+
     // Remove start button to replace with Player score.
     startContainer.removeChild(startBtn);
     var scoreText = document.createElement('span');
     scoreText.textContent = `Score: 0`;
     startContainer.appendChild(scoreText);
+
+    // Set up questions and answers when start button is clicked
+    var question = document.getElementById(Math.floor(Math.random()*Math.floor(25)));
+    question.removeAttribute(`class`, `d-none`);
+    var answerchoices = question.querySelectorAll('p');
 
     // Set player score to zero
     var score = 0;
@@ -35,8 +46,9 @@ startBtn.addEventListener(`click`, function() {
     }
 
     // Store time information for an interval function
-    let secondsLeft = 30;
+    let secondsLeft = 5;
     timeEl.textContent = `Time: ${secondsLeft}`;
+
 
     // Set a time interval to allow answers to be selected once the start button is clicked
     var timerInterval = setInterval(function() {
@@ -44,14 +56,31 @@ startBtn.addEventListener(`click`, function() {
         if(secondsLeft === 0) {
             alert(`Time's up!`);
             clearInterval(timerInterval);
+
+        // store score here
+            storeScore()
+
             window.location = './highscore.html'
         }
         secondsLeft--
         question.addEventListener('click', clickAnswer)
     }, 1000)
 
-    // Set up questions and answers when start button is clicked
-    var question = document.getElementById(Math.floor(Math.random()*Math.floor(25)+1));
-    question.removeAttribute(`class`, `d-none`);
-    var answerchoices = question.querySelectorAll('p');
+
+    
+    
+    function storeScore () {
+        scores.push(score);
+        localStorage.setItem("Quiz scores", JSON.stringify(scores));
+    }
+
+
+
+    function getScore() {
+        var storedScore = JSON.parse(localStorage.getItem(`Quiz scores`));
+    
+        if (storedScore !== null) {
+            scores = storedScore;
+        }
+    }
 })
